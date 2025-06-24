90b5e17ed160e6ff51900f9e3626736ecea8dbc8
Daniel Dias <danieldias.it98@gmail.com>
2025-06-24 01:38:17 +0100

    Created controller-service-repository

---
+++ springboot-backend/src/main/java/com/example/springboot_backend/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.springboot_backend.controller;
+import com.example.springboot_backend.service.UserService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+
+}
---
+++ springboot-backend/src/main/java/com/example/springboot_backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.springboot_backend.repository;
+import com.example.springboot_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
